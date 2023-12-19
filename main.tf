5ea5e8cb1573c39e18f9297c8a264f97d6ced9c3
kishanmv1 <venkatkishan117@gmail.com>
2023-12-19 16:11:09 +0530

    my third terraform commit removed keyname

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "abhi_terraform_server" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
-    key_name = "mac_keypair"
+    
   
 }
