5631812ce88d742670b5fbab76c9f9ea979fd66f
kishanmv1 <venkatkishan117@gmail.com>
2023-12-20 11:13:12 +0530

    my seventh terraform commit changes made in all tf files

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "abhi_terraform_server" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-    subnet_id = var.subnet_id_value  
-    key_name = "mac_keypair"
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id     = aws_subnet.terraform_subnet1.id
+  key_name      = "mac_keypair"
+  vpc_security_group_ids = ["${aws_security_group.terraform_sg.id}"]
 }
+
--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "ami_value" {
-    description = "value for the ami"
+  description = "value for the ami"
 }
 
 variable "instance_type_value" {
-    description = "value for the instance_type"
+  description = "value for the instance_type"
 }
 
 variable "subnet_id_value" {
-    description = "value for the subnet_id"
+  description = "value for the subnet_id"
 }
